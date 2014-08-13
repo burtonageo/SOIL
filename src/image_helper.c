855e92101b99780987f4d17dd61a0019b214ed3f
George Burton <burtonageo@gmail.com>
2014-08-13 16:02:23 +0100

    Added divide-by-zero check

--- src/image_helper.c
+++ src/image_helper.c
@@ -144,7 +144,8 @@ int
 				{
 					sum_value += orig[index + v*width*channels + u*channels];
 				}
-				resampled[j*mip_width*channels + i*channels + c] = sum_value / block_area;
+				resampled[j*mip_width*channels + i*channels + c] = sum_value /
+					(block_area == 0 ? 1 : block_area);
 			}
 		}
 	}
